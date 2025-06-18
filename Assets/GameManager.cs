91258d2bd1ccaa66bf7f656c27f5d026f69f9394
LinBoSam0 <linbosam0@gmail.com>
2025-06-18 21:13:44 +0800

    666
    得分，過關

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,7 +6,10 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public int starCount = 0;
-    public Text starText; // 把 UI 元件拖進來
+    private int totalStars = 0;
+
+    public Text starText;     // 顯示星星數量的 UI Text
+    public GameObject winPanel; // 過關畫面（顯示「過關！」的 UI）
 
     void Awake()
     {
@@ -16,15 +19,39 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    void Start()
+    {
+        // 開始時自動找到所有星星
+        totalStars = GameObject.FindGameObjectsWithTag("star").Length;
+
+        // 一開始先隱藏過關畫面
+        if (winPanel != null)
+            winPanel.SetActive(false);
+
+        UpdateUI();
+    }
+
     public void CollectStar()
     {
         starCount++;
         UpdateUI();
+
+        if (starCount >= totalStars)
+        {
+            ShowWinPanel();
+        }
     }
 
     void UpdateUI()
     {
         if (starText != null)
-            starText.text = "Stars: " + starCount.ToString();
+            starText.text = "Stars: " + starCount + "/" + totalStars;
+    }
+
+    void ShowWinPanel()
+    {
+        Debug.Log("過關了！");
+        if (winPanel != null)
+            winPanel.SetActive(true);
     }
 }
